525af0b5c2d96d5e77ec392191d39112552013dd
Peacock-Co <pavaoleonardo@hotmail.com>
2020-01-09 18:16:07 +0100

    destructuring arrays makes shorter code, chained assignments

--- spec/koans.js
+++ spec/koans.js
@@ -145,8 +145,8 @@ describe('a template string, is wrapped in ` (backticks) instead of \' or ". ', 
 
   describe('by default, behaves like a normal string', function () {
     it('just surrounded by backticks', function () {
-      /*let str = ??????*/
-      //expect(str).toEqual('like a string');
+      let str = `like a string`
+      expect(str).toEqual('like a string');
     });
 
   });
@@ -157,13 +157,13 @@ describe('a template string, is wrapped in ` (backticks) instead of \' or ". ', 
   describe('can evaluate variables, which are wrapped in "${" and "}"', function () {
 
     it('e.g. a simple variable "${x}" just gets evaluated', function () {
-      let evaluated = `x=x`
-      //expect(evaluated).toBe('x=' + x);
+      let evaluated = `x=${x}`
+      expect(evaluated).toBe('x=' + x);
     });
 
     it('multiple variables get evaluated too', function () {
-      var evaluated = `x+y`;
-      //expect(evaluated).toBe(x + '+' + y);
+      var evaluated = `${x}+${y}`;
+      expect(evaluated).toBe(x + '+' + y);
     });
 
   });
@@ -171,16 +171,16 @@ describe('a template string, is wrapped in ` (backticks) instead of \' or ". ', 
   describe('can evaluate any expression, wrapped inside "${...}"', function () {
 
     it('all inside "${...}" gets evaluated', function () {
-      var evaluated = Number(`x+y`);
-      //expect(evaluated).toBe(x+y);
+      var evaluated = Number(`${x+y}`);
+      expect(evaluated).toBe(x + y);
     });
 
     it('inside "${...}" can also be a function call', function () {
       function getSchool() {
         return 'Ironhack';
       }
-      var evaluated = `getSchool()`;
-      //expect(evaluated).toBe('Ironhack');
+      var evaluated = `${getSchool()}`;
+      expect(evaluated).toBe('Ironhack');
     });
 
   });
@@ -194,12 +194,22 @@ describe('The object literal allows for new shorthands. ', () => {
 
   describe('with variables', () => {
     it('the short version for `{y: y}` is {y}', () => {
-      /*.....*/
-      //expect(short).toEqual({y: y});
+      short = {
+        y
+      };
+      expect(short).toEqual({
+        y: y
+      });
     });
     it('works with multiple variables too', () => {
-      /*.....*/
-      //expect(short).toEqual({x: x, y: y});
+      short = {
+        x,
+        y
+      };
+      expect(short).toEqual({
+        x: x,
+        y: y
+      });
     });
   });
 
@@ -208,13 +218,21 @@ describe('The object literal allows for new shorthands. ', () => {
     const func = () => func;
 
     it('using the name only uses it as key', () => {
-      /*.......*/
-      //expect(short).toEqual({func: func});
+      short = {
+        func
+      };
+      expect(short).toEqual({
+        func: func
+      });
     });
 
     it('a different key must be given explicitly, just like before ES6', () => {
-      /*.......*/
-      //expect(short).toEqual({otherKey: func});
+      short = {
+        otherKey: func
+      }
+      expect(short).toEqual({
+        otherKey: func
+      });
     });
   });
 
@@ -223,35 +241,37 @@ describe('The object literal allows for new shorthands. ', () => {
 describe('destructuring arrays makes shorter code. ', () => {
 
   it('extract value from array, e.g. extract 0 into x like so `let [x] = [0];`', () => {
-    let firstValue = [1];
-    //expect(firstValue).toEqual(1);
+    let [firstValue] = [1];
+    expect(firstValue).toEqual(1);
   });
 
   it('swap two variables, in one operation', () => {
     let [x, y] = ['ax', 'why'];
-    [x, y] = [x, y];
-    //expect([x, y]).toEqual(['why', 'ax']);
+    [x, y] = [y, x];
+    expect([x, y]).toEqual(['why', 'ax']);
   });
 
   it('leading commas', () => {
     const all = ['ax', 'why', 'zet'];
-    const [z] = all;
-    //expect(z).toEqual('zet');
+    const [, , z] = all;
+    expect(z).toEqual('zet');
   });
 
   it('extract from nested arrays', () => {
     const user = [
       ['Some', 'One'], 23
     ];
-    const [firstName, surname, age] = user;
+    const [
+      [firstName, surname], age
+    ] = user;
 
     const expected = 'Some One = 23 years';
-    //expect(`${firstName} ${surname} = ${age} years`).toEqual(expected);
+    expect(`${firstName} ${surname} = ${age} years`).toEqual(expected);
   });
 
   it('chained assignments', () => {
     let c, d;
-    // let a, b = c, d = [1, 2];
+    //let a, b = c, d = [1, 2];
     //expect([a, b, c, d]).toEqual([1, 2, 1, 2]);
   });
 
